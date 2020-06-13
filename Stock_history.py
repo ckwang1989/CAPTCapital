4faa1f17beba0653368b5050b8356f36e77cc135
Dary (Hao-Chang) Cheng <Dary.Cheng@monolithicpower.com>
2020-06-14 02:20:49 +0800

    updated A.stock_history B.Technical index

---
+++ Stock_history.py
@@ -0,0 +1,23 @@
+import requests
+# import pandas as pd
+from datetime import datetime
+import time
+import os
+import yfinance as yf # option https://aroussi.com/post/python-yahoo-finance
+import pandas as pd
+
+class sum():
+    def Stock_single_no_data(self, stock_TW):
+
+        filepath=os.getcwd() + '\\stock_temp'
+        if not os.path.isdir(filepath):
+            os.mkdir(filepath)
+        stock = yf.Ticker(stock_TW)
+        df =stock.history(period="max")
+        df=df.reset_index()
+        df.to_csv(os.getcwd() + '\\stock_temp' + '\\file_%s.csv' %stock_TW,index='Date')
+        return df
+
+
+# if __name__ == '__main__':
+#     df= sum().Stock_single_no_data('AMD')
